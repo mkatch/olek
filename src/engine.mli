65e02bd14747d186329fa340eb8e1107db7b8f47
Marcin Kaczmarek <mkacz91@gmail.com>
2015-01-19 22:09:13 +0100

    Refactored game entry point

--- src/engine.mli
+++
@@ -1,7 +0,0 @@
-type state
-
-val init : w:int -> h:int -> fps:int -> state
-
-val quit : unit -> unit
-
-val iter : state -> state option
--- src/game.ml
+++ src/game.ml
@@ -1,8 +1,128 @@
+open Core.Std
+open Utils
+open Sdlevent
+open Sdlkey
+open Printf
+open Mind
+
+type time = {
+  frame : int;
+  t_ms : int;
+  dt_ms : int;
+}
+
+type state = {
+  canvas : Canvas.t;
+  room : Room.t;
+  bodies : Body.t list;
+  minds : mind list;
+  time : time;
+}
+
+let rec make_objs n mind =
+  if n <= 0 then []
+  else
+    let x = Random.float 800. and y = Random.float 600. in
+    let (body, mindi, cmds) = Object.create (make_v x y) mind in
+    (body, mindi) :: make_objs (n - 1) mind
+
+let init ~w ~h ~fps =
+  Sdl.init [`VIDEO];
+  let canvas = Canvas.init ~w:w ~h:h in
+  let objs = make_objs 1 Minds.dummy in
+  let bodies, minds = List.unzip objs in
+  let room = Room.load "room" in
+  let room = Room.add_tiles_layer room in
+  let time = { 
+    frame = 0;
+    t_ms = Sdltimer.get_ticks ();
+    dt_ms = 1000 / fps;
+  } in {
+    canvas = canvas;
+    bodies = bodies;
+    minds = minds;
+    room = room;
+    time = time;
+  }
+
+let quit () = Sdl.quit ()
+
+let env_of_state state =
+  let time = state.time in
+  Env.{
+    t = Float.of_int (time.frame * time.dt_ms) /. 1000.;
+    dt = Float.of_int time.dt_ms /. 1000.;
+    tiles = Room.tiles state.room;
+  }
+
+let draw state =
+  let c = state.canvas in
+  Canvas.draw_room c state.room;
+  List.iter ~f:(Canvas.draw_body c) state.bodies;
+  Canvas.flip c
+
+let update_time state =
+  let time = state.time in
+  let next_t_ms = time.t_ms + time.dt_ms in
+  let current_t_ms = Sdltimer.get_ticks () in
+  let delay_ms = max (next_t_ms - current_t_ms) 0 in
+  Sdltimer.delay delay_ms;
+  let new_t_ms = Sdltimer.get_ticks () in
+  let new_time = { time with frame = time.frame + 1; t_ms = new_t_ms } in
+  { state with time = new_time }
+
+let advance_sprites state =
+  let bodies = List.map ~f:(Body.advance_sprite state.time.t_ms) state.bodies in
+  { state with bodies }
+
+let process_event state event =
+  match event with
+  | QUIT
+  | KEYDOWN {keysym = KEY_ESCAPE} -> None
+  | _ -> Some state
+
+let process_events state =
+  let rec aux state events =
+    match Sdlevent.poll () with
+    | None -> (Some state, List.(concat_map ~f:Objevent.of_sdl_event (rev events)))
+    | Some event -> match process_event state event with
+      | None -> (None, [])
+      | Some state' -> aux state' (event :: events) in
+  aux state []
+
+let process_obj_events events = function
+  | None -> None
+  | Some state ->
+    let env = env_of_state state in
+    let objs_cmdss = List.map2_exn ~f:(Object.dispatch_events events env)
+      state.bodies state.minds in
+    let bodies, minds, cmdss = unzip3 objs_cmdss in
+    Some { state with bodies = bodies; minds = minds }
+
+let think = process_obj_events [Objevent.NextFrame]
+
+let focus = function
+  | None -> None
+  | Some state ->
+    let c = state.canvas in
+    let b = List.hd_exn state.bodies in
+    Some { state with canvas = Canvas.focus (Body.pos b) c }
+
+let iter state =
+  draw state;
+  let state = update_time state in
+  let state = advance_sprites state in
+  let state, obj_events = process_events state in
+  let state = process_obj_events obj_events state in
+  let state = think state in
+  let state = focus state in
+  state
+
 let main () =
   Random.self_init ();
   let rec loop f t state =
-    match Engine.iter state with
-    | None -> Engine.quit ()
+    match iter state with
+    | None -> quit ()
     | Some state ->
       let c = Unix.time () in
       if t +. 1. <= c then begin
@@ -10,6 +130,6 @@ let main () =
         loop 1 c state
       end else
         loop (f + 1) t state
-  in loop 1 (Unix.time ()) (Engine.init ~w:800 ~h:600 ~fps:30)
+  in loop 1 (Unix.time ()) (init ~w:800 ~h:600 ~fps:30)
 
 let () = main ()
